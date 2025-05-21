e14a9a385b60c0e1be4c4b32d19c9968f20dc036
murratta <slavakim.kim1953@gmail.com>
2025-05-21 18:29:26 +0300

    config.py

--- config.py
+++ config.py
@@ -1,14 +1,2 @@
 STUDENT_ID = "Kim_c2796806"
-SOURCES = [
-    "https://www.ukrinform.ua/rss/all.xml",
-    "https://www.pravda.com.ua/rss/news/",
-    "https://tsn.ua/rss/news.rss",
-    "https://interfax.com.ua/rss/main.xml",
-    "https://ua.korrespondent.net/rss/ukraine",
-    "https://zn.ua/rss/all.xml",
-    "https://tyzhden.ua/rss/",
-    "https://24tv.ua/rss/novyny_ukraine.xml",
-    "http://feeds.bbci.co.uk/news/world/rss.xml",
-    "http://feeds.reuters.com/reuters/topNews",
-    "https://www.coindesk.com/arc/outboundfeeds/rss/",
-]
+SOURCES = []
